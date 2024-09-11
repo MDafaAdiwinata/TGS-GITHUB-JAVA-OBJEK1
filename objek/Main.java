5fd85a43f670d81af39347be9e24342abf32328c
M Dafa Adiwinata <dafaadi2106@gmail.com>
2024-09-11 18:38:01 +0700

    Add files via upload

---
+++ objek/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.dicoding.javafundamental.objek;
+
+/**
+ *
+ * @author adiwi
+ */
+public class Main {
+    public static void main(String[] args) {
+        Hewan objekHewan = new Hewan();
+        objekHewan.cetakNama("Elang");
+    }
+}
